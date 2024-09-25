9d6d444c669223a6dc9b5d1064a22b58204103f7
nigussolomon <nigussolomon444@gmail.com>
2024-09-25 14:49:38 +0300

    added the custom host

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,8 @@ module Raise
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.hosts << "www.qr.raisemanufacturing.com"
+    config.hosts << "qr.raisemanufacturing.com"
     config.hosts << "raise-2.onrender.com"
     config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
